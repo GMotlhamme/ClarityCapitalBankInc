5f1abc2fc14066228162260e494b53023b88b555
Gomolemo Motlhamme <gotelltaylor@gmail.com>
2026-06-07 23:45:29 +0200

    Add API auth test and update npm/NuGet dependencies
    Added PaymentApiTest to verify unauthorized access to /api/Employee/AllPayments. Included Microsoft.AspNetCore.Mvc.Testing in test project. Updated npm dependencies (axios, react-router) and added agent-base, https-proxy-agent.

---
+++ BankApi.Tests/PaymentApiTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.Web.CodeGeneration.Design;
+using System.Net;
+using System.Net.Http.Json;
+using Xunit;
+
+namespace BankApi.Tests;
+
+public class PaymentApiTest : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly HttpClient _client;
+
+    public PaymentApiTest(WebApplicationFactory<Program> factory)
+    {
+        _client = factory.CreateClient();
+    }
+
+    [Fact]
+    public async Task GetPayments_Should_Return_Unauthorized_Without_Token()
+    {
+        var response = await _client.GetAsync("/api/Employee/AllPayments");
+
+        Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
+    }
+}
